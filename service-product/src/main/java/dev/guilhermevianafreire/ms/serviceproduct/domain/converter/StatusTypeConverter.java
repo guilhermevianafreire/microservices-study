bd6dff773b0f091a02aacc3fad7e65ceedb8a723
Guilherme Viana Freire <guilherme.viana.freire@gmail.com>
2022-04-29 14:04:01 -0300

    Converter annotated so that it can be auto applied to all entities

--- service-product/src/main/java/dev/guilhermevianafreire/ms/serviceproduct/domain/converter/StatusTypeConverter.java
+++ service-product/src/main/java/dev/guilhermevianafreire/ms/serviceproduct/domain/converter/StatusTypeConverter.java
@@ -1,9 +1,11 @@
 package dev.guilhermevianafreire.ms.serviceproduct.domain.converter;
 
 import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
 
 import dev.guilhermevianafreire.ms.serviceproduct.domain.constants.StatusType;
 
+@Converter(autoApply = true)
 public class StatusTypeConverter implements AttributeConverter<StatusType, Integer> {
 
 	@Override
