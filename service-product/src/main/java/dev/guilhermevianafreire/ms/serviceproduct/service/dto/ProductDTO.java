7d30f440fdee9f9b0118d44fb7b8daab7a0e0289
Guilherme Viana Freire <guilherme.viana.freire@gmail.com>
2022-05-17 01:19:51 -0300

    Added validation groups.

--- service-product/src/main/java/dev/guilhermevianafreire/ms/serviceproduct/service/dto/ProductDTO.java
+++ service-product/src/main/java/dev/guilhermevianafreire/ms/serviceproduct/service/dto/ProductDTO.java
@@ -7,18 +7,34 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
-import dev.guilhermevianafreire.ms.serviceproduct.domain.constants.StatusType;
 import lombok.Builder;
 
 @Builder
 public record ProductDTO(
-                         UUID id,
-                         @NotBlank @Size(max = 100) String name,
+                         @NotNull(groups = {
+                                             ProductUpdateGroup.class
+                         }) UUID id,
+                         @NotBlank(groups = {
+                                              ProductSaveGroup.class,
+                                              ProductUpdateGroup.class
+                         }) @Size(max = 100, groups = {
+                                                        ProductSaveGroup.class,
+                                                        ProductUpdateGroup.class
+                         }) String name,
                          String description,
-                         @NotNull StatusType status,
+                         @NotNull(groups = {
+                                             ProductSaveGroup.class,
+                                             ProductUpdateGroup.class
+                         }) Integer statusCode,
                          Instant createdDate,
                          String createdBy,
                          Instant lastModifiedDate,
-                         String lastModifiedBy) {
+                         String lastModifiedBy){
+
+  public interface ProductUpdateGroup {
+  }
+
+  public interface ProductSaveGroup {
+  }
 
 }
