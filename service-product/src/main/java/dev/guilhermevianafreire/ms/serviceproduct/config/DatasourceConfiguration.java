49f07675eac1e159ea68a4643bb448e64d44dfb9
Guilherme Viana Freire <guilherme.viana.freire@gmail.com>
2022-05-18 20:45:53 -0300

    MongoDB datasource configuration

--- service-product/src/main/java/dev/guilhermevianafreire/ms/serviceproduct/config/DatasourceConfiguration.java
+++ service-product/src/main/java/dev/guilhermevianafreire/ms/serviceproduct/config/DatasourceConfiguration.java
@@ -1,14 +1,25 @@
 package dev.guilhermevianafreire.ms.serviceproduct.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.MongoDbFactory;
+import org.springframework.data.mongodb.MongoTransactionManager;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @EnableJpaRepositories({
                          "dev.guilhermevianafreire.ms.serviceproduct.repository"
 })
+@EnableMongoRepositories
 @EnableTransactionManagement
 public class DatasourceConfiguration {
+
+//    @Bean
+//    MongoTransactionManager transactionManager(MongoDatabaseFactory dbFactory) {
+//        return new MongoTransactionManager(dbFactory);
+//    }
 
 }
