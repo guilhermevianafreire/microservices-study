b1cee3f9e26fb858cfa32f2718344308ae19ce3c
Guilherme Viana Freire <guilherme.viana.freire@gmail.com>
2022-05-18 20:44:29 -0300

    Adjusted default ComponentScan basePackages to scan components from the shared project.

---
+++ service-product/src/main/java/dev/guilhermevianafreire/ms/serviceproduct/config/ComponentScanConfig.java
@@ -0,0 +1,9 @@
+package dev.guilhermevianafreire.ms.serviceproduct.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = {"dev.guilhermevianafreire.ms"})
+public class ComponentScanConfig {
+}
