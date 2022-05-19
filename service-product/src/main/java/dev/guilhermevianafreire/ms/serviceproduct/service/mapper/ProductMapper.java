b91c2f113f78b1d8b923dfbc4cd7c6c85bc47ed5
Guilherme Viana Freire <guilherme.viana.freire@gmail.com>
2022-05-19 00:08:27 -0300

    Improved formatting.

--- service-product/src/main/java/dev/guilhermevianafreire/ms/serviceproduct/service/mapper/ProductMapper.java
+++ service-product/src/main/java/dev/guilhermevianafreire/ms/serviceproduct/service/mapper/ProductMapper.java
@@ -9,24 +9,24 @@ import java.util.List;
 @Mapper
 public interface ProductMapper {
 
-  @Mapping(target = "status", expression = "java(StatusType.lookupByCode(dto.statusCode()).orElseThrow())")
-  Product toEntity(ProductDTO dto);
+    @Mapping(target = "status", expression = "java(StatusType.lookupByCode(dto.statusCode()).orElseThrow())")
+    Product toEntity(ProductDTO dto);
 
-  @Mapping(target = "statusCode", source = "status.code")
-  ProductDTO toDto(Product entity);
+    @Mapping(target = "statusCode", source = "status.code")
+    ProductDTO toDto(Product entity);
 
-  List<Product> toEntity(List<ProductDTO> dtoList);
+    List<Product> toEntity(List<ProductDTO> dtoList);
 
-  List<ProductDTO> toDto(List<Product> entityList);
+    List<ProductDTO> toDto(List<Product> entityList);
 
-  @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
-  void partialUpdate(@MappingTarget Product destination,
-                     Product origin);
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void partialUpdate(@MappingTarget Product destination,
+                       Product origin);
 
-  @Mapping(target = "id", ignore = true)
-  @Mapping(target = "status", expression = "java(StatusType.lookupByCode(dto.statusCode()).orElseThrow())")
-  void updateEntityWithDto(@MappingTarget Product entity,
-                           ProductDTO dto);
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "status", expression = "java(StatusType.lookupByCode(dto.statusCode()).orElseThrow())")
+    void updateEntityWithDto(@MappingTarget Product entity,
+                             ProductDTO dto);
 
 
 }
